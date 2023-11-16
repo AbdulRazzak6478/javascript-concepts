15f4bc9e98e9f8006f3515ff912b60e5ab4812b1
Abdul Razzak <abdulrazzak74832@gmail.com>
2023-11-17 00:11:54 +0530

    javascript concepts

---
+++ Advanced_js/defer1.js
@@ -0,0 +1,2 @@
+let res=confirm("this is a program of using defer or not ");
+alert("your answer is :     "+res);
---
+++ basic/intro.js
@@ -0,0 +1,11 @@
+function msg() {
+  document.write(
+    "Hello this is a first page of practicing and applying javascript in to web page"
+  );
+  var a = 23;
+  var b = 34;
+  var d = a * b;
+  var e=a-b;
+  var c = a + b;
+  document.write(d);
+}
